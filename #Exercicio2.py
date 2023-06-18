de8bbd374dc689d8af9924c72e1d5c28a5e1079e
Felipe Kttu <kttudev@gmail.com>
2023-06-18 01:45:20 -0300

    Update #Exercicio2.py

--- #Exercicio2.py
+++ #Exercicio2.py
@@ -1,24 +1,27 @@
-"""Exercicio2
-Elaborar um programa que lê um número inteiro entre 1 e 7 e exibe o dia da semana correpondente.
+#Exercicio2
+'''Elaborar um programa que lê um número inteiro entre 1 e 7 e exibe o dia da semana correpondente.
 O programa deve repetir até que o usuário digite um número fora desse intervalo,caso isso aconteça o algortimo
-mostra uma mensagem informando "Número inválido !!"""
+mostra uma mensagem informando "Número inválido !!'''
 
-N = 1
-while N >= 1 and N <= 7:
-    N = int(input('Digite o dia da semana(Número):'))
-    if N == 1:
+num = 1
+
+while num >= 1 and num <= 7:
+
+    num = int(input('Digite o dia da semana(Número):'))
+
+    if num == 1:
         print('Hoje é Domingo !!')
-    elif N == 2:
+    elif num == 2:
         print('Hoje é Segunda !!')
-    elif N == 3:
+    elif num == 3:
        print('Hoje é Terça !!')
-    elif N == 4:
+    elif num == 4:
         print('Hoje é Quarta !!')
-    elif N == 5:
+    elif num == 5:
         print('Hoje é Quinta !!')
-    elif N == 6:
+    elif num == 6:
         print('Hoje é Sexta !!')
-    elif N == 7:
+    elif num == 7:
         print('Hoje é Sabádo')
-if N <= 0 or N > 7:
+if num <= 0 or num > 7:
     print('Número Invalido !!')

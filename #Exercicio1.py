ac2491db695d2fd12e885330969f63c480e4b070
Felipe Kttu <kttudev@gmail.com>
2023-06-18 01:44:03 -0300

    Update #Exercicio1.py

--- #Exercicio1.py
+++ #Exercicio1.py
@@ -1,11 +1,15 @@
-"""Exercicio1
-Elaborar um programa que solicita varias temperaturas em Graus Celsius, para cada temperatura inserida,
+#Exercicio1
+'''Elaborar um programa que solicita varias temperaturas em Graus Celsius, para cada temperatura inserida,
 o programa deve converter a para graus Fahrenheit e Kelvin e mostrar na tela
-o programa termina quando a temperatura inserida for menor que -5"""
-T = 0
-while (T > -5):
-    T = float(input('Digite uma temperatura em Celsius que queira converter para Fahrenheit e Kelvin:'))
-    F = T * 1.8 + 32
-    K = T + 273
-    print('{}° Celsius são {}° Fahrenheit e {}°Kelvin'.format(T,F,K))
+o programa termina quando a temperatura inserida for menor que -5 '''
+
+temperatura = 0
+
+while (temperatura > -5):
+
+    temperatura = float(input('Digite uma temperatura em Celsius que queira converter para Fahrenheit e Kelvin:'))
+    fahrenheit = temperatura * 1.8 + 32
+    kelvin = temperatura + 
+    
+    print('{}° Celsius são {}° Fahrenheit e {}°Kelvin'.format(temperatura, fahrenheit, kelvin))
     

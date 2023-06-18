7d28f4e0212e8152d9abf625a3da8e5376a0d08f
Felipe Kttu <kttudev@gmail.com>
2023-06-18 01:51:45 -0300

    Update #Exercicio5.py

--- #Exercicio5.py
+++ #Exercicio5.py
@@ -1,14 +1,20 @@
-#Exercicio5
-#Elaborar um programa que lê um número N inteiro maior que 2 (caso N não for maior que 2 deve solicitar outro
-# número).Logo após o programa deve exibir o quadrado e o cubo de 2 até N 
-N = 0
-C = 1
-X = 0
-while N < 2:
-    N = int(input('Digite um número maior que 2:'))
-for C in range(2,N):
-    N = C 
-    Q = N * N
-    CB = N * N * N
-    C += 1
-    print('O seu numero é {} Seu quadrado é {} Seu cubo é {}'.format(N,Q,CB))
+#Exercicio5#Exercicio5
+
+'''Elaborar um programa que lê um número num inteiro maior que 2 (caso o número não for maior que 2 deve solicitar outro
+número). Logo após o programa deve exibir o quadrado e o cubo de 2 até o número '''
+
+num = 0
+cont = 1
+
+while num < 2:
+
+    num = int(input('Digite um número maior que 2:'))
+
+for cont in range(2,num):
+
+    num = cont 
+    quadrado = num * num
+    cubo = num * num * num
+    cont += 1
+
+    print('O seu numero é {} Seu quadrado é {} Seu cubo é {}'.format(num,quadrado,cubo))

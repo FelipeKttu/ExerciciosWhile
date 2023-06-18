f9751d87fb6c211ee9a7d9c85a5bf591dec3aaf8
Felipe Kttu <kttudev@gmail.com>
2023-06-18 02:07:58 -0300

    Update #Exercicio_10.py

--- #Exercicio_10.py
+++ #Exercicio_10.py
@@ -1,13 +1,18 @@
 #Exercicio10
 '''Elaborar um programa que contem uma lista com 15 elementos. Essa lista deve ser preenchida pelo usuário,
 porém não deve conter valores repitidos.Exibir no final da lista'''
-C = 0
-L = []
-Ultimo = 0
-while C < 16:
+
+cont = 0
+lista = []
+ultimo = 0
+
+while cont < 16:
     N = int(input('Digite um número para adicionar na lista:'))
-    if N != Ultimo:
-        L.append(N)
-    Ultimo = N
-    C = C + 1
-print('A lista é {}'.format(L))
+
+    if N != ultimo:
+        lista.append(N)
+
+    ultimo = N
+    cont = cont + 1
+    
+print('A lista é {}'.format(lista))

f2f8fb9f4a9171254d287b962b67e571b7212b08
Felipe Kttu <kttudev@gmail.com>
2023-06-18 01:53:46 -0300

    Update #Exercicio6.py

--- #Exercicio6.py
+++ #Exercicio6.py
@@ -1,11 +1,17 @@
 #Exercicio6
+
 """Elaborar um programa que solicite um número (entre 10 e 15). Se o usuario digitar um número diferente, 
 o programa deve mostrar a mensagem "Entrada invalida" e solicitar um número novamente.Se o digitar correto o programa 
 deve mostrar a raiz quadrada desse número e o programa termina"""
+
 from math import sqrt
-X = int(input('Digite um número entre 10 e 15:'))
-while X < 10 or X > 15:
+
+num = int(input('Digite um número entre 10 e 15:'))
+
+while num < 10 or num > 15:
   print('Entrada invalida !!')
-  X = int(input('Digite um número entre 10 e 15:'))
-R = sqrt(X)
-print('A raiz de {} é {:.2f}'.format(X,R))
+
+  num = int(input('Digite um número entre 10 e 15:'))
+
+raiz = sqrt(num)
+print('A raiz de {} é {:.2f}'.format(num, raiz))

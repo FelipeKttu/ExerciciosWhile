72ddf2ec0b889ccd7dd4257e797cd56e429f7682
Felipe Kttu <kttudev@gmail.com>
2023-06-18 02:01:41 -0300

    Update #Exercicio8.py

--- #Exercicio8.py
+++ #Exercicio8.py
@@ -1,14 +1,20 @@
 #Exercicio8
-'''Elaborar um programa que contem uma lista com N elementos.essa lista deve ser prenchida pelo usuário e só deve
+'''Elaborar um programa que contem uma lista com numero elementos, essa lista deve ser prenchida pelo usuário e só deve
 conter números inteiros positivos e pares.Caso o usúario digite o número 1 a repetição termina imediatamente.
 exibir no final os elementos da lista'''
-L = []
-N = 0
-C = 0
-E = int(input('Digite quantos elementos deve ter lista:'))
-while C >= 0 and C < E and N != 1:
-    N = int(input('Digite um número inteiro e par para adicionar na lista:'))
-    if N % 2 == 0:
-      L.append(N)
-      C += 1
-print('A lista é {}'.format(L))
+
+lista = []
+numero = 0
+cont = 0
+elementos = int(input('Digite quantos elementos deve ter lista:'))
+
+while cont >= 0 and cont < elementos and numero != 1:
+
+    numero = int(input('Digite um número inteiro e par para adicionar na lista:'))
+
+    if numero % 2 == 0:
+      lista.append(numero)
+      cont += 1
+
+print('A lista é {}'.format(lista))
+      
